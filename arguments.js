41d39d3cc57fd5ed8545c0b9471c728d75a81d8f
Salaatsarkar <salaatsarkar0501@gmail.com>
2026-02-09 18:38:51 +0400

    argument array object practice

---
+++ arguments.js
@@ -0,0 +1,6 @@
+function add(num1 , num2)
+{
+    console.log(num1, num2);
+    console.log(arguments); // ai arguments shudhu function er moddhei likha jabe.
+}
+add(2, 3,6 ,7,8,9,3,4,7);
---
+++ object.js
@@ -0,0 +1,9 @@
+const husband ={
+    name : "Azaan sarkaar",
+    age : 23,
+    maritalStatus : "married",
+    favouriteFood:["monda", "biriyani" , "cha","kacchi", "rosomalai"],
+    Children : {name : "amraa" , age :1, skinColor:"white"},
+}
+console.log(husband);
+console.log(typeof(husband))
